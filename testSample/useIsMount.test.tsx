855f68069bfba3175f21fb1b6dbc4637c4004cb8
mmmmmmmmn <mygithubemailllll@gmail.com>
2020-07-30 06:47:46 +0900

    add useIsMountTest

---
+++ testSample/useIsMount.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
+
+const useIsMount = () => {
+    const isMount = React.useRef(false)
+
+    React.useEffect(() => {
+        isMount.current = true
+
+        return () => {
+            isMount.current = false
+        }
+    }, [])
+
+    return isMount
+}
+
+describe('useIsMount', () => {
+    const mount = () => {
+        const {
+            result: { current: isMountRef },
+            unmount,
+        } = renderHook(useIsMount)
+
+        return { isMountRef, unmount }
+    }
+
+    it('should be true while mounting', () => {
+        const { isMountRef } = mount()
+
+        expect(isMountRef.current).toBe(true)
+    })
+    it('should be false when not mounted', () => {
+        const { isMountRef, unmount } = mount()
+
+        unmount()
+
+        expect(isMountRef.current).toBe(false)
+    })
+})
