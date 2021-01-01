5145262e4636ab5e3770ff24acc6ab532584a2b2
mmmmmmmmn <mygithubemailllll@gmail.com>
2021-01-01 15:03:00 +0900

    update lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{md,json,scss,css,js,ts,tsx}': ['prettier -w', 'git add'],
-    '*.{js,ts,tsx}': ['eslint --max-warnings=0 --fix', 'git add'],
+    '*.{md,json,scss,css,js,ts,tsx}': ['prettier -w'],
+    '*.{js,ts,tsx}': ['eslint --max-warnings=0 --fix'],
 }
