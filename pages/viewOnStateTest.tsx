9d792a0447e63b566854f7bf8a4420ae6e1ac72b
mmmmmmmmn <mygithubemailllll@gmail.com>
2021-01-30 11:11:59 +0900

    add viewOnStateTest

---
+++ pages/viewOnStateTest.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const ViewOnStateTest: NextPage = () => {
+    const [content, setSomeState] = React.useState<React.ReactElement | null>(<div>initial</div>)
+    const isContent1 = React.useRef(false)
+
+    const toggle = () => {
+        isContent1.current = !isContent1.current
+        setSomeState(isContent1.current ? Content1({}) : Content2({}))
+    }
+
+    return (
+        <>
+            <button onClick={toggle}>toggle</button>
+            content: {content}
+        </>
+    )
+}
+
+const Content1: React.FC = () => <h1>content1</h1>
+const Content2: React.FC = () => <h2>content2</h2>
+
+export default ViewOnStateTest
