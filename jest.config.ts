ad33bea78e708aa1a887b68729410519dc24a10c
mmmmmmmmn <51997429+mmmmmmmmn@users.noreply.github.com>
2021-05-26 21:04:44 +0900

    jest.config.js → jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
     testEnvironment: 'jsdom',
     preset: 'ts-jest',
     transform: { '\\.[jt]sx?$': 'ts-jest' },
     verbose: true,
 }
+
+export default config
